4f61d970b8dc11f4e9403d70cc29f42f353c505c
Larry Li <larry.zy.li@gmail.com>
2017-12-15 12:35:40 -0600

    finished plugging in controller to server

--- server/index.js
+++ server/index.js
@@ -15,12 +15,14 @@ massive(process.env.CONNECTION_STRING).then(db => {
     app.set("db", db);
 });
 
-// app.get('/api/bins/:id') // get all shelf by id -- when user selects witch shelf to see
+//shelf API
+app.get("/api/shelf/:id", mc.getShelf); // get all shelf by id -- when user selects witch shelf to see
 
-// app.get('/api/bin/:id') // get bin by id -- when a user selects a bin
-// app.put('/api/bin/:id') // update bin by id -- when a user edits an existing item
-// app.delete('/api/bin/:id') // delete bin by id -- when a user deletes an existing item
-// app.post('/api/bin/:id') // create bin with id -- when a user adds an item to an empty bin
+//bin API
+app.get("/api/bin/:id", mc.getBin); // get bin by id -- when a user selects a bin
+app.put("/api/bin/:id", mc.updateBin); // update bin by id -- when a user edits an existing item
+app.delete("/api/bin/:id", mc.deleteBin); // delete bin by id -- when a user deletes an existing item
+app.post("/api/bin/:id", mc.createBin); // create bin with id -- when a user adds an item to an empty bin
 
 const port = 3001;
 app.listen(port, () => {
